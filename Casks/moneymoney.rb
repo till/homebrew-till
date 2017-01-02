5245d758cc9709836cfe98d43cb7864e0d74867b
till <till@php.net>
2017-01-02 21:48:16 +0100

    Fix: add sha

--- Casks/moneymoney.rb
+++ Casks/moneymoney.rb
@@ -1,5 +1,6 @@
 cask 'moneymoney' do
   version '2.2.15'
+  sha256 'be09bf954c7253d160b68dc775a5084c51b2440e590c033a96db728e9b08c052'
 
   url 'https://service.moneymoney-app.com/1/MoneyMoney.zip'
   appcast 'https://service.moneymoney-app.com/1/Appcast.xml',
